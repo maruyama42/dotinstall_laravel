37272a5e7fff6378757f63f7a124466da76faaee
Your Name <you@example.com>
2019-04-19 15:14:11 +0000

    first commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index() {
+      // $posts = \App\Post::all();
+      // $posts = Post::all();
+      // $posts = Post::orderBy('created_at', 'desc')->get();
+      $posts = Post::latest()->get();
+      // $posts = [];
+      // dd($posts->toArray()); // dump die
+      // return view('posts.index', ['posts' => $posts]);
+      return view('posts.index')->with('posts', $posts);
+    }
+
+    
+    
+    // public function show($id) {
+    public function show(Post $post) {
+        \URL::forceSchema('https');
+      // $post = Post::find($id);
+      // $post = Post::findOrFail($id);
+      return view('posts.show')->with('post', $post);
+      
+    }
+    
+}
